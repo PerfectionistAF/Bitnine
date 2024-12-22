4b8fb666633d38a0b4395fa94f613281d8243201
PerfectionistAF <ihabsohaila@yahoo.com>
2024-12-22 18:02:47 +0200

    SUCCESS: Display all forecasts per city

--- Voda/my-app/src/app/app.component.ts
+++ Voda/my-app/src/app/app.component.ts
@@ -11,26 +11,29 @@ export class AppComponent {
   title = 'my-app';
   
   city: string = '';
-  date: string | undefined;
-  temperatureCelsius: number = 0;
-  temperatureFahrenheit: number = 0;
-  humidity: number = 0;
+  //date: string | undefined;
+  //temperatureCelsius: number = 0;
+  //temperatureFahrenheit: number = 0;
+  //humidity: number = 0;
+
+  forecasts: any[] = [];  ///array of forecasts per city
 
   constructor(private weatherService: WeatherServiceService) {}
 
   ngOnInit() {
-    this.getCityForecast(5); //Test with ids
+    this.getCityForecast(9); //Test with ids
   }
 
   getCityForecast(cityId: number) {
     this.weatherService.getCityForecast(cityId).subscribe(
       data => {
         this.city = data.city;
-        const firstForecast = data.forecast[0];
-        this.date = firstForecast.date;
-        this.temperatureCelsius = firstForecast.temperatureCelsius;
-        this.temperatureFahrenheit = firstForecast.temperatureFahrenheit;
-        this.humidity = firstForecast.humidity;
+        this.forecasts = data.forecast;
+        //const firstForecast = data.forecast;//[0];
+        //this.date = firstForecast[0].date;
+        //this.temperatureCelsius = firstForecast[0].temperatureCelsius;
+        //this.temperatureFahrenheit = firstForecast[0].temperatureFahrenheit;
+        //this.humidity = firstForecast[0].humidity;
       },
       error => {
         console.error('Error fetching city forecast', error);
